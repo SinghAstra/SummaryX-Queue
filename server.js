396ee6e50cc45e1bdc7e40cc2680bbff675bd185
Astra <abhaypratapsinghwd@gmail.com>
2024-06-08 15:52:31 +0530

    rendering views

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
 app.listen(3000, () => {
