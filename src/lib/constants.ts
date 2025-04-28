75f62ce115e75f112ee062eb608c869d4a284472
Astra <abhaypratapsinghwd@gmail.com>
2025-04-28 08:59:52 +0530

    Route to Clean Jobs

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,8 @@ export const QUEUES = {
   REPOSITORY: "repository-queue",
   DIRECTORY: "directory-queue",
   SUMMARY: "summary-queue",
+  LOG: "log-queue",
 };
 
 export const FILE_BATCH_SIZE_FOR_PRISMA_TRANSACTION = 50;
-export const FILE_BATCH_SIZE_FOR_AI_SHORT_SUMMARY = 10;
+export const FILE_BATCH_SIZE_FOR_AI_SUMMARY = 10;
